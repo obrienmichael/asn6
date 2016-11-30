6dacfbc40a8c52e0f8ab276a6a89b1d1cccdf1f2
Sydney <sfpugh@loyola.edu>
2016-11-30 18:40:39 -0500

    Add files via upload

---
+++ Entry.java
@@ -0,0 +1,31 @@
+package asn6;
+
+//This is our code.
+//Michael O'Brien, Sydney Pugh
+//CS312
+
+/**
+ * Generic hashtable element type
+ */
+public class Entry<E>
+{
+	private E value;
+	
+	/**
+	 * Parameterized Entry constructor
+	 * @param v - value of entry
+	 */
+	public Entry(E v)
+	{
+		value = v;
+	}
+	
+	/**
+	 * Method to generate string representation of Entry
+	 */
+	@Override
+	public String toString()
+	{
+		return value.toString();
+	}
+}
