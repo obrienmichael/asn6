9f4569663f3dce21e694cd98072b3e6e64c14ce8
Sydney <sfpugh@loyola.edu>
2016-11-30 18:40:51 -0500

    Add files via upload

--- OAHashtable.java
+++ OAHashtable.java
@@ -4,38 +4,37 @@ package asn6;
 // Michael O'Brien, Sydney Pugh
 // CS312
 
-import java.util.Random;
+//import java.util.Random;
 
 /**
  * Hashtable using open addressing
  */
-public class OAHashtable 
+public class OAHashtable<E> 
 {
-	private Integer[] table;
+	private Entry<E>[] table;
 	private double loadThreshold;
 	private int numKeys;
 	private int numDeletes;
-	private final int DELETED = -1;
-	private final int UNUSED = -1;
-
+	private final Entry<E> DELETED = new Entry<E>(null);
+	//private final int UNUSED = -1;
 
 	/**
 	 * Parameterized OAHashtable constructor
 	 * @param size - size 
 	 * @param load - load threshold
 	 */
+	@SuppressWarnings("unchecked")
 	public OAHashtable(int size, double load)
 	{
-		table = new Integer[size];
+		table = new Entry[size];
 		loadThreshold = load;
 	}
-
 	
 	/**
 	 * Method to add an object into the hash table
 	 * @param it - object to be inserted
 	 */
-	public void add(Integer it)
+	public void add(Entry<E> it)
 	{
 		int index = it.hashCode() % table.length;
 
@@ -60,35 +59,33 @@ public class OAHashtable
 		}
 	}
 	
-	
 	/**
 	 * Method to remove an object from the hash table
 	 * @param it - object to be removed
 	 */
-	public void remove(Integer it)
+	public void remove(Entry<E> it)
 	{
 	}
-	
 	
 	/**
 	 * Method to rehash the table, doubling the size of the table
 	 * and transferring only non-null and non-deleted elements
 	 * into new array
 	 */
+	@SuppressWarnings("unchecked")
 	private void rehash()
 	{
-		Integer[] oldTable = table;
-		table = new Integer[oldTable.length * 2];
+		Entry<E>[] oldTable = table;
+		table = new Entry[oldTable.length * 2];
 		numKeys = 0;
 		numDeletes = 0;
 
-		for(Integer integer : oldTable)
+		for(Entry<E> element : oldTable)
 		{
-			if(integer != null && integer != DELETED)
-				add(integer);
+			if(element != null && element != DELETED)
+				add(element);
 		}
 	}
-
 
 	/**
 	 * Method to print out contents of the hash table
@@ -100,12 +97,11 @@ public class OAHashtable
 			System.out.print(" " + table[i]);
 		System.out.println();
 	}
-
 	
 	public static void main(String [] args)
 	{
-		Random r = new Random(42);
-		OAHashtable ht = new OAHashtable(5, 0.1);
+		//Random r = new Random(42);
+		OAHashtable<Integer> ht = new OAHashtable<Integer>(5, 0.1);
 
 		/*for(int i=0; i<20; i++)
 	    {
@@ -113,11 +109,12 @@ public class OAHashtable
 	      ht.add(x);
 	      System.out.print(" " + x);
 	    }*/
+		
 		ht.dumpTable();
 
-		ht.add(3);
-		ht.add(4);
-		ht.add(5);
+		ht.add(new Entry<Integer>(3));
+		ht.add(new Entry<Integer>(4));
+		ht.add(new Entry<Integer>(5));
 
 		ht.dumpTable();
 	}
